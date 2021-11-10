119325b00649acfff16d40678020196e288e1a98
João F. Ferreira <joao@joaoff.com>
2021-11-10 10:42:28 +0000

    fix: correct geth version in Maian's Dockerfile fix: script that calls maian for bytecode analysis

--- utils/docker/smartbugs-maian/scripts/run_maian_bytecode.sh
+++ utils/docker/smartbugs-maian/scripts/run_maian_bytecode.sh
@@ -3,6 +3,6 @@
 FILENAME=$1
 
 cd /MAIAN/tool
-python3 maian.py -c 0 -bs ${FILENAME}
-python3 maian.py -c 1 -bs ${FILENAME}
-python3 maian.py -c 2 -bs ${FILENAME}
+python3 maian.py -c 0 -b ${FILENAME}
+python3 maian.py -c 1 -b ${FILENAME}
+python3 maian.py -c 2 -b ${FILENAME}
