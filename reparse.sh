61f08f74c7f108571d5f0cc11d9feca49ccf2722
gsalzer <salzer@logic.at>
2022-02-15 00:27:28 +0100

    changes to HoneyBadger, EthBMC

--- reparse.sh
+++ reparse.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne 2 ]; then
 	cat <<END
 Usage:   $0 <directory tree containing result.log> <parser to use>
-	 Re-parses each result.log in the directory tree, generating result.new.json.
+	 Parses all result.log files into files named result.new.json.
          If result.json exists, it is used to initialize result.new.json.
 Example: $0 smartbugs/results/vandal Vandal
 END
@@ -12,19 +12,20 @@ fi
 
 DIR="$1"
 PARSERS="src/output_parser"
-PARSER="$PARSERS/$2.py"
+PARSER="$2.py"
 
 if [ ! -d "$DIR" ]; then
 	echo "$DIR does not exist"
 	exit 1
 fi
 
+cd "$PARSERS"
+
 if [ ! -f "$PARSER" ]; then
 	echo "$PARSER not found"
 	exit 1
 fi
 
-cd "$PARSERS"
 source ../../venv/bin/activate
 for LOG in `find "$DIR" -name result.log`; do
 	echo "$LOG"
