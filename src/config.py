06dc9afef06ad224e52a0924607bc3382b916f86
gsalzer <salzer@logic.at>
2022-03-14 14:12:57 +0100

    BASE_DIR variable instead of multiple references to __file__

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
-import os, yaml
+import os, yaml, pathlib
 
-TOOLS_CFG_PATH = os.path.join(os.path.dirname(__file__), '..', 'config', 'tools')
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+TOOLS_CFG_PATH = os.path.join(BASE_DIR, 'config', 'tools')
 TOOLS = {}
 for f in os.listdir(TOOLS_CFG_PATH):
     tool_name,ext = os.path.splitext(f)
@@ -13,14 +15,13 @@ for f in os.listdir(TOOLS_CFG_PATH):
                 print(exc)
 TOOL_CHOICES = TOOLS.keys()
 
-DATASETS_CFG_PATH = os.path.join(os.path.dirname(__file__), '..', 'config', 'datasets.yaml')
-DATASETS_PARENT = os.path.join(os.path.dirname(__file__), '..')
+DATASETS_CFG_PATH = os.path.join(BASE_DIR, 'config', 'datasets.yaml')
+DATASETS_PARENT = BASE_DIR
 with open(DATASETS_CFG_PATH, 'r') as ymlfile:
     try:
         DATASETS = yaml.safe_load(ymlfile)
     except yaml.YAMLError as exc:
         print(exc)
-
 DATASET_CHOICES = []
 for name,location in DATASETS.items():
     if isinstance(location, dict) and 'subsets' in location:
@@ -36,5 +37,5 @@ def get_remote_info(info):
     return (info['url'], info['local_dir'], info['subsets'])
 
 import pandas
-VULNERABILITY_MAP_PATH = os.path.join(os.path.dirname(__file__), 'output_parser', 'sarif_vulnerability_mapping.csv')
+VULNERABILITY_MAP_PATH = os.path.join(BASE_DIR, 'src', 'output_parser', 'sarif_vulnerability_mapping.csv')
 VULNERABILITY_MAP = pandas.read_csv(VULNERABILITY_MAP_PATH)
