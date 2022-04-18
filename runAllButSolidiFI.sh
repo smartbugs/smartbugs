b9344bb0b986154f0512d9165d5b9afe60ee5f60
JDinis <joao.t.dinis@gmail.com>
2022-04-18 09:49:37 -0700

    script update

--- runAllButSolidiFI.sh
+++ runAllButSolidiFI.sh
@@ -1,9 +1,9 @@
-python3 smartBugs.py --tool all --dataset access_control --unique-sarif-output
-python3 smartBugs.py --tool all --dataset arithmetic --unique-sarif-output
-python3 smartBugs.py --tool all --dataset bad_randomness --unique-sarif-output
-python3 smartBugs.py --tool all --dataset denial_of_service --unique-sarif-output
-python3 smartBugs.py --tool all --dataset front_running --unique-sarif-output
-python3 smartBugs.py --tool all --dataset reentrancy --unique-sarif-output
-python3 smartBugs.py --tool all --dataset short_addresses --unique-sarif-output
-python3 smartBugs.py --tool all --dataset time_manipulation --unique-sarif-output
-python3 smartBugs.py --tool all --dataset unchecked_ll_calls --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset access_control --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset arithmetic --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset bad_randomness --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset denial_of_service --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset front_running --unique-sarif-output
+/data/software/Python-3.9.10/pythonsmartBugs.py --tool all --dataset reentrancy --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset short_addresses --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset time_manipulation --unique-sarif-output
+/data/software/Python-3.9.10/python smartBugs.py --tool all --dataset unchecked_ll_calls --unique-sarif-output
