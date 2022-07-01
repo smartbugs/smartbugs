f1c4c1b5a924a1f56bd3498805e90e9553e33e3c
tdurieux <durieuxthomas@hotmail.com>
2022-07-01 14:16:07 +0200

    deploy a local blockchain in pakala image to avoid connection issues

--- utils/docker/smartbugs-pakala/scripts/runPakala.sh
+++ utils/docker/smartbugs-pakala/scripts/runPakala.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-URL=$1
-FILENAME=$2
+FILENAME=$1
 
-export WEB3_PROVIDER_URI="$1"
+geth --dev --http --http.api eth,web3,personal,net &
+export WEB3_PROVIDER_URI="http://127.0.0.1:8545"
 cat ${FILENAME} | pakala - --exec-timeout 1500 --analysis-timeout 300
