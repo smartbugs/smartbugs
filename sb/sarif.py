e2e0c0239e3483d9aa1c7013ffc7cfb776d8405f
Gernot Salzer <salzer@logic.at>
2022-11-17 12:56:03 +0000

    results2csv script (list entries)

--- sb/sarif.py
+++ sb/sarif.py
@@ -1,4 +1,4 @@
-import sb.tools
+import sb.tools, sb.utils
 
 
 def sarify(tool, findings):
@@ -82,30 +82,8 @@ def result_info(tool_id, finding):
     return result_dict
 
 
-def str2label(s):
-    """Convert string to label.
-
-    - leading non-letters are removed
-    - trailing characters that are neither letters nor digits ("other chars") are removed
-    - sequences of other chars within the string are replaced by a single underscore
-    """
-    l = ""
-    separator = False
-    has_started = False
-    for c in s:
-        if c.isalpha() or (has_started and c.isdigit()):
-            has_started = True
-            if separator:
-                separator = False
-                l += "_"
-            l += c
-        else:
-            separator = has_started
-    return l
-
-
 def rule_id(tool_id, fname):
-    return f"{str2label(tool_id)}_{str2label(fname)}"
+    return f"{sb.utils.str2label(tool_id)}_{str2label(fname)}"
 
 
 def rule_shortDescription(info_finding):
