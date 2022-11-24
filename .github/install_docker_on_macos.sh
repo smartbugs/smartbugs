9c2e4d65c938fdc9ab32fe7e34fdbf2935ee3685
Gernot Salzer <salzer@logic.at>
2022-11-24 15:22:53 +0000

    yet another try

--- .github/install_docker_on_macos.sh
+++ .github/install_docker_on_macos.sh
@@ -4,6 +4,5 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask docker
 sudo /Applications/Docker.app/Contents/MacOS/Docker --unattended --install-privileged-components
 open -a /Applications/Docker.app --args --unattended --accept-license
 echo "We are waiting for Docker to be up and running. It can take over 2 minutes..."
-while ! /Applications/Docker.app/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
-
-
+#while ! /Applications/Docker.app/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
+sleep 5m
