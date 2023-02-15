db9cd6d8d0dd7a005b67f0c5d3d870fc32ccc9e2
Gernot Salzer <salzer@logic.at>
2023-02-15 19:56:19 +0000

    Fix: timeout parameter for ethainter and madmax

--- .github/github_results.sh
+++ .github/github_results.sh
@@ -5,7 +5,5 @@
 # as a reference for comparing the results of the workflow with
 
 rm -rf results/github
-./smartbugs -t all -f 'samples/SimpleDAO.*' --runid github --json --main
-./results2csv -x start duration -- results/*/github \
-    | sed 's/,Transaction_Order_Dependency//' \
-    > .github/results-ubuntu.csv
+./smartbugs -t all -f 'samples/SimpleDAO.*' --runid github --json --main --timeout 300
+./results2csv -x start duration -- results/*/github > .github/results-ubuntu.csv
