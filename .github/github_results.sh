a0fdc0c38d4a626cba8d1d3f43ffcb218d25e703
Gernot Salzer <salzer@logic.at>
2023-06-11 20:22:54 +0200

    Fix binaries for sample contract BecToken

--- .github/github_results.sh
+++ .github/github_results.sh
@@ -5,13 +5,13 @@
 # as a reference for comparing the results of the workflow with
 
 #rm -rf results/*/github-sol
-./smartbugs -t all -f 'samples/SimpleDAO.sol' --runid github-sol --json --main --timeout 180
+./smartbugs -t all -f 'samples/SimpleDAO.sol' --runid github-sol --sarif --main --timeout 180
 ./results2csv -x start duration -- results/*/github-sol | sed '/confuzzius/s/".*"//' > .github/results-ubuntu-sol.csv
 
 #rm -rf results/*/github-rt
-./smartbugs -t all -f 'samples/SimpleDAO.rt.hex' --runid github-rt --json --main --timeout 180
+./smartbugs -t all -f 'samples/SimpleDAO.rt.hex' --runid github-rt --sarif --timeout 180
 ./results2csv -x start duration -- results/*/github-rt > .github/results-ubuntu-rt.csv
 
 #rm -rf results/*/github-hx
-./smartbugs -t all -f 'samples/SimpleDAO.hex' --runid github-hx --json --main --timeout 180
+./smartbugs -t all -f 'samples/SimpleDAO.hex' --runid github-hx --sarif --timeout 180
 ./results2csv -x start duration -- results/*/github-hx > .github/results-ubuntu-hx.csv
