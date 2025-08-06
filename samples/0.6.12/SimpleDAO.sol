a77938318bd9ebd8532fd80ad076595d05414ee7
Gernot Salzer <salzer@logic.at>
2025-08-06 21:22:21 +0200

    Update sample contracts to major 0.5, 0.6, 0.7, 0.8 versions

---
+++ samples/0.6.12/SimpleDAO.sol
@@ -0,0 +1,29 @@
+/*
+ * @source: http://blockchain.unica.it/projects/ethereum-survey/attacks.html#simpledao
+ * Upgraded from Solidity 0.4 to 0.6, vulnerability may no longer exist
+ * @author: -
+ * @vulnerable_at_lines: 21
+ */
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+contract SimpleDAO {
+  mapping (address => uint) public credit;
+
+  function donate(address to) public payable {
+    credit[to] += msg.value;
+  }
+
+  function withdraw(uint amount) public {
+    if (credit[msg.sender]>= amount) {
+      // <yes> <report> REENTRANCY
+      (bool res,) = msg.sender.call{value: amount}("");
+      credit[msg.sender]-=amount;
+    }
+  }
+
+  function queryCredit(address to) public view returns (uint){
+    return credit[to];
+  }
+}
