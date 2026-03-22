0e698c746ae2295b9581bdc22cc5745121474034
Stephan Klein <sklein.at@gmail.com>
2026-03-22 19:27:41 +0100

    Fix bug where not all findings were added in case Vulhunter does not report on exact position, as it is is the case for Bytecode analysis. Also sync instance len of bytecode script to solidity script.

--- tools/vulhunter/scripts/do_bytecode.sh
+++ tools/vulhunter/scripts/do_bytecode.sh
@@ -3,7 +3,7 @@
 FILENAME="$1"
 TIMEOUT="$2"
 
-CMD="python3 /app/main/main.py --contract $FILENAME --filetype bytecode --model-dir models --instance-len 10 --report /app/output.pdf"
+CMD="python3 /app/main/main.py --contract $FILENAME --filetype bytecode --model-dir models --instance-len 20 --report /app/output.pdf"
 
 echo "Executing $CMD"
 $CMD
